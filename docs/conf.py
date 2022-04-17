4053544d9d1e7f914f60e26e56ace1b00ad082e1
Jonahs11 <jonahsilverman11@gmail.com>
2022-04-17 15:07:20 -0400

    Removed myst_parser

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = 'AtlasXomics'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser", "sphinx_rtd_theme"
+ "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
